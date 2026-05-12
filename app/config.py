2002e63e886551bd3de699f396c1a8cbc4a753d8
MitoNacho <mitonacho9@gmail.com>
2026-05-12 17:22:49 +0200

    fix railway db driver

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+<<<<<<< HEAD
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -22,3 +23,14 @@ if DATABASE_URL:
             "postgresql+psycopg://",
             1
         )
+=======
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# FIX RAILWAY + SQLALCHEMY + PSYCOPG3
+if DATABASE_URL:
+    if DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+
+    if DATABASE_URL.startswith("postgresql://"):
+        DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+>>>>>>> aa583fe (fix railway db driver)
