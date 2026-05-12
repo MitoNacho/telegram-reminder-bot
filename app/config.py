32543c6ffea9bb8fb7629a51ff3fa68de9b814c0
Nacho Naves <134378740+MitoNacho@users.noreply.github.com>
2026-05-12 17:42:16 +0200

    Add files via upload

--- app/config.py
+++ app/config.py
@@ -1,18 +1,24 @@
 import os
-
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# FIX RAILWAY + SQLALCHEMY + PSYCOPG3
+# 🔥 FIX IMPORTANTE PARA EVITAR psycopg2
 if DATABASE_URL:
     if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgres://",
+            "postgresql+psycopg://",
+            1
+        )
 
     if DATABASE_URL.startswith("postgresql://"):
-        DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgresql://",
+            "postgresql+psycopg://",
+            1
+        )
